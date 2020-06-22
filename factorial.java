251e53e4cb57546ed42fcc1043e29a2e47140649
saitripuralekkala <45476807+saitripuralekkala@users.noreply.github.com>
2020-06-22 17:25:41 +0530

    factorial.java

---
+++ factorial.java
@@ -0,0 +1,10 @@
+class factorial{
+	public static void main(string[] args)
+	{
+	int n=new java.util.scanner(system.in).nextInt(),b;
+	for(i=1;i<n;i++)
+	{
+	b*=i;
+	}
+	system.out.println("%d",b);
+}
